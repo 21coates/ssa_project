735d196e3280a0e1aa1c83d5880e6f601151cdce
“Oliver <21coates@oakhill.nsw.edu.au>
2025-12-04 11:44:15 +1100

    User Authentication

--- chipin/admin.py
+++ chipin/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Group
 
-@admin.register(Group)
+@admin.register(Group) # Register the admin model
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("name", "admin")
